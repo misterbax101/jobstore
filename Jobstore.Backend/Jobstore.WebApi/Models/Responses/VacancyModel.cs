fffe76faafe32c9465cab18525b2a16d1cd98de7
Yaroslav Borovyi <borovyi0808@gmail.com>
2019-11-17 21:02:51 +0200

    add methods

---
+++ Jobstore.Backend/Jobstore.WebApi/Models/Responses/VacancyModel.cs
@@ -0,0 +1,20 @@
+﻿
+using Jobstore.Infrastructure.Entities;
+using System;
+
+namespace Jobstore.WebApi.Models.Responses
+{
+    public class VacancyModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Descripion { get; set; }
+        public string CompanyName { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public VacancyType Type { get; set; }
+        public double SalaryValue { get; set; }
+        public string SalaryCurrency { get; set; }
+        public string OwnerId { get; set; }
+        public string OwnerName { get; set; }
+    }
+}
